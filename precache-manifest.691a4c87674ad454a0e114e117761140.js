2bf4bd57f360aaa0684635716fba7c4082ca6c98
Richard Magdaluyo <rpmagdaluyo@gmail.com>
2020-01-30 21:16:28 -0800

    Updates

---
+++ precache-manifest.691a4c87674ad454a0e114e117761140.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ea297d1b321efc520fa2bd9f83f9fd",
+    "url": "/richardarc18/index.html"
+  },
+  {
+    "revision": "969846932a31fb9aedda",
+    "url": "/richardarc18/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "05a0839099dc3896d81f",
+    "url": "/richardarc18/static/js/2.a49c80c2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/richardarc18/static/js/2.a49c80c2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "969846932a31fb9aedda",
+    "url": "/richardarc18/static/js/main.68dfa2e0.chunk.js"
+  },
+  {
+    "revision": "f99d9cd88140af1a77c8",
+    "url": "/richardarc18/static/js/runtime-main.32afea1d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/richardarc18/static/media/logo.5d5d9eef.svg"
+  }
+]);
